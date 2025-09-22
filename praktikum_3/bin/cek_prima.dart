adcf209847146a808f3b0c833a788ddea0f0439c
= <=>
2025-09-22 19:34:59 +0700

    feat: implementasi logika cek bilangan prima

--- praktikum_3/bin/cek_prima.dart
+++ praktikum_3/bin/cek_prima.dart
@@ -7,6 +7,19 @@ void main() {
   
   stdout.write("Masukkan bilangan: ");
   int n = int.parse(stdin.readLineSync()!);
+  // Cek bilangan prima
+  bool prima = true;
 
-  print("Input Anda: $n");
+  if (n < 2) {
+    prima = false;
+  } else {
+    for (int i = 2; i <= n ~/ 2; i++) {
+      if (n % i == 0) {
+        prima = false;
+        break;
+      }
+    }
+  }
+
+  print("Hasil cek untuk $n, prima? $prima");
 }
