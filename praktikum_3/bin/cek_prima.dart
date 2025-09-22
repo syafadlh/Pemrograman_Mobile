22fdb491f4475dd83ba1d64f77e96784281cb47b
= <=>
2025-09-22 20:04:57 +0700

    feat: improve code readability with additional comments

--- praktikum_3/bin/cek_prima.dart
+++ praktikum_3/bin/cek_prima.dart
@@ -2,17 +2,21 @@
 import 'dart:io';
 
 void main() {
+  // Judul program
   print("=== Program Cek Bilangan Prima ===");
   print("Project Dart pertama berhasil dijalankan!");
-  
+
+  // Input bilangan
   stdout.write("Masukkan bilangan: ");
   int n = int.parse(stdin.readLineSync()!);
   // Cek bilangan prima
   bool prima = true;
 
+  // Logika pengecekan bilangan prima
   if (n < 2) {
     prima = false;
   } else {
+    // Cek pembagi dari 2 hingga n/2
     for (int i = 2; i <= n ~/ 2; i++) {
       if (n % i == 0) {
         prima = false;
