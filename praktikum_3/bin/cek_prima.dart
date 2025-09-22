8ae36a65da72823943d410d0db8df10c7d8caef1
= <=>
2025-09-22 13:59:08 +0700

    feat: tambah fitur input angka

--- praktikum_3/bin/cek_prima.dart
+++ praktikum_3/bin/cek_prima.dart
@@ -1,5 +1,9 @@
 // cek_prima.dart
+import 'dart:io';
+
 void main() {
-  print("=== Program Cek Bilangan Prima ===");
-  print("Project Dart pertama berhasil dijalankan!");
+  stdout.write("Masukkan bilangan: ");
+  int n = int.parse(stdin.readLineSync()!);
+
+  print("Input Anda: $n");
 }
