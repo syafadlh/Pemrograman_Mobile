a8a17fc7b1da82b79ac39491b90460bfde614f4d
= <=>
2025-09-22 19:30:05 +0700

    feat: add introductory messages to prime number check program

--- praktikum_3/bin/cek_prima.dart
+++ praktikum_3/bin/cek_prima.dart
@@ -2,6 +2,9 @@
 import 'dart:io';
 
 void main() {
+  print("=== Program Cek Bilangan Prima ===");
+  print("Project Dart pertama berhasil dijalankan!");
+  
   stdout.write("Masukkan bilangan: ");
   int n = int.parse(stdin.readLineSync()!);
 
