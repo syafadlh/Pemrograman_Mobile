2fc7ccb9227c79abc3a8a34f78abe51952d98723
= <=>
2025-09-23 19:43:38 +0700

    Praktikum 1: Eksperimen Tipe Data List

---
+++ praktikum_4/bin/praktikum_4.dart
@@ -0,0 +1,5 @@
+import 'package:praktikum_4/praktikum_4.dart' as praktikum_4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${praktikum_4.calculate()}!');
+}
---
+++ praktikum_4/test/praktikum_4_test.dart
@@ -0,0 +1,8 @@
+import 'package:praktikum_4/praktikum_4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
