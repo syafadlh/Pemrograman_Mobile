84435367f55faea792af437eaed750faf488af81
= <=>
2025-09-30 10:47:03 +0700

    chore: rename folder

--- praktikum_4/bin/praktikum_4.dart
+++
@@ -1,5 +0,0 @@
-import 'package:praktikum_4/praktikum_4.dart' as praktikum_4;
-
-void main(List<String> arguments) {
-  print('Hello world: ${praktikum_4.calculate()}!');
-}
--- praktikum_4/test/praktikum_4_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:praktikum_4/praktikum_4.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
